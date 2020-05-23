764a953627ac2704604f1bd213b34da4c74303e2
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-05-22 18:19:27 -0700

    Build enemy intelligence

--- ascii_game/ascii_game.py
+++ ascii_game/ascii_game.py
@@ -40,11 +40,13 @@ class GameObject():
         elif direction == "east" and col < self.game.num_cols - 1:
             col += 1
         # If the collision is valid (True), update position
-        if self.handle_collision(grid[row][col]):
+        moved = self.handle_collision(grid[row][col])
+        if moved:
             self.row = row
             self.col = col
         grid[self.row][self.col] = self
         game.draw_grid()
+        return moved
     def handle_collision(self, collision_object):
         if collision_object == " ":
             return True
@@ -69,6 +71,7 @@ class GameObject():
 
 
 
+
 class Barrier(GameObject):
     def __init__(self, row, col, game):
         super().__init__("0", row, col, game)
@@ -76,13 +79,13 @@ class Barrier(GameObject):
 
 
 class Weapon(GameObject):
-    def __init__(self, name, player_row, player_col, facing, lifetime, game):
+    def __init__(self, name, player_row, player_col, facing, lifetime, size, game):
         self.name = name
         self.type = "WEAPON"
         self.game = game
 
         row_col = self.set_row_col(player_row, player_col, facing)
-        if row_col:
+        if row_col and size > 0:
             game.weapons.append(self)
             for enemy in game.enemies:
                 if enemy.row == self.row and enemy.col == self.col:
@@ -90,6 +93,7 @@ class Weapon(GameObject):
             game.grid[self.row][self.col] = self
             self.frame_to_destroy = game.frame + lifetime
             game.draw_grid()
+            Weapon(name, self.row, self.col, facing, lifetime, size-1, game)
 
     def set_row_col(self, player_row, player_col, facing):
         self.row = player_row
@@ -116,6 +120,7 @@ class Weapon(GameObject):
 
 
 
+
 class Player(GameObject):
     def __init__(self, name, row, col, game):
         super().__init__(name, row, col, game)
@@ -124,7 +129,7 @@ class Player(GameObject):
         self.attack_speed = int(self.game.framerate / 4)  # 1/4 second
         self.cooldown = 0
         self.facing = "east"
-        self.weapon_size = 5
+        self.weapon_size = 2
     def process_command(self, c):
         directions = {"w": "north", "a": "west", "s": "south", "d": "east"}
         if c in directions:
@@ -139,9 +144,7 @@ class Player(GameObject):
         print("You have died.")
         self.game.running = False
     def attack(self):
-        w = Weapon("+", self.row, self.col, self.facing, self.attack_speed, self.game)
-        for i in range(1, self.weapon_size):
-            w = Weapon("+", w.row, w.col, self.facing, self.attack_speed, self.game)
+        w = Weapon("+", self.row, self.col, self.facing, self.attack_speed, self.weapon_size, self.game)
         self.cooldown = self.attack_speed+1  # One frame longer than weapon
 
 
@@ -160,7 +163,11 @@ class Enemy(GameObject):
     def do_action(self):
           self.random_move()
     def random_move(self):
-        self.move(random.choice(["north", "south", "east", "west"]))
+        move_choices = ["north", "south", "west", "east"]
+        # Try again if the move is unsuccessful
+        tries = 5
+        while tries > 0 and not self.move(random.choice(move_choices)):
+            tries -= 1
         self.frame_to_act += self.speed
     def object_collision(self, collision_object):
         if collision_object.type == "PLAYER":
