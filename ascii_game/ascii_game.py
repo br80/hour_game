7edfab346d639bdbe7f3eb6a0bff30a49b303cfd
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-05-22 17:27:19 -0700

    Add working weapon

--- ascii_game/ascii_game.py
+++ ascii_game/ascii_game.py
@@ -49,6 +49,11 @@ class GameObject():
     def handle_collision(self, collision_object):
         if collision_object == " ":
             return True
+        elif collision_object.type == "BARRIER":
+            return False
+        elif collision_object.type == "WEAPON":
+            self.die()
+            return False
         return self.object_collision(collision_object)
 
     def object_collision(self, collision_object):
@@ -72,9 +77,25 @@ class Barrier(GameObject):
 
 
 class Weapon(GameObject):
-    def __init__(self, name, row, col, game):
+    def __init__(self, name, row, col, lifetime, game):
         super().__init__(name, row, col, game)
-        self.type = "PLAYER"
+        self.type = "WEAPON"
+        game.weapons.append(self)
+        for enemy in game.enemies:
+            if enemy.row == row and enemy.col == col:
+                enemy.die()
+        game.grid[row][col] = self
+        self.frame_to_destroy = game.frame + lifetime
+        game.draw_grid()
+    def act(self, frame):
+        if frame >= self.frame_to_destroy:
+            self.die()
+    def die(self):
+        self.game.grid[self.row][self.col] = " "
+        self.game.weapons.remove(self)
+        self.game.draw_grid()
+
+
 
 
 class Player(GameObject):
@@ -82,12 +103,22 @@ class Player(GameObject):
         super().__init__(name, row, col, game)
         self.type = "PLAYER"
         self.game.grid[row][col] = self
+        self.attack_speed = int(self.game.framerate / 1)  # 1/1 second
+        self.cooldown = 0
+    def process_command(self, c):
+        if c in ["w", "a", "s", "d"]:
+            self.move(c)
+        elif c == "p":
+            self.attack()
     def object_collision(self, collision_object):
         self.die()
         return False
     def die(self):
         print("You have died.")
         self.game.running = False
+    def attack(self):
+        w = Weapon("+", self.row, self.col+1, self.attack_speed, self.game)
+        self.cooldown = self.attack_speed
 
 
 class Enemy(GameObject):
@@ -95,28 +126,32 @@ class Enemy(GameObject):
         super().__init__(name, row, col, game)
         self.game.enemies.append(self)
         self.type = "ENEMY"
-        self.speed = speed
-        self.frame_to_act = int(3600 / self.speed)
+        self.speed = self.game.framerate * 100 / speed
         self.game.grid[row][col] = self
+        self.frame_to_act = int(1 * self.speed)
     def act(self, frame):
         if frame >= self.frame_to_act:
             self.do_action()
-            self.frame_to_act += int(3600 / self.speed)
+            self.frame_to_act += int(1 * self.speed)
     def do_action(self):
-        self.random_move()
+          self.random_move()
     def random_move(self):
         self.move(random.choice(["w","a","s","d"]))
+        self.frame_to_act += self.speed
     def object_collision(self, collision_object):
         if collision_object.type == "PLAYER":
             collision_object.die()
             return True
         elif collision_object.type == "ENEMY":
-            return True
+            return False
         else:
             self.die()
             return False
     def die(self):
         self.game.enemies.remove(self)
+        occupant = self.grid[self.row][self.col]
+        if occupant == self:
+            occupant = " "
 
 
 class Game:
@@ -128,30 +163,34 @@ class Game:
         for i in range(self.num_rows):
             self.grid.append([" "] * self.num_cols)
 
+        self.framerate = 60
         self.frame = 0
         self.running = True
 
         self.player = Player("Br80", 0, 0, self)
+        self.cooldown = 0
+        self.weapons = []
 
         self.enemies = []
-        Enemy("X", 5, 0, 200, self)
-        Enemy("X", 5, 1, 200, self)
-        Enemy("Y", 6, 0, 200, self)
-        Enemy("Y", 6, 1, 200, self)
-        Enemy("X", 7, 0, 200, self)
+        Enemy("X", 5, 0, 100, self)
+        Enemy("X", 5, 1, 400, self)
+        Enemy("Y", 6, 0, 400, self)
+        Enemy("Y", 6, 1, 400, self)
+        Enemy("X", 7, 0, 400, self)
+
 
     def draw_grid(self):
         clear_screen()
         for row in self.grid:
             char_row = [str(c)[0] for c in row]
             print(" _ ".join(char_row))
+        print(self.player.cooldown)
 
 
     def run(self):
         frame = 0
         t = time.time()
-        framerate = 60
-        frame_time = 1 / framerate
+        frame_time = 1 / self.framerate
 
         kb = kbhit.KBHit()
 
@@ -163,16 +202,19 @@ class Game:
             # self.draw_grid()
             self.frame += 1
             start_time = time.time()
-            movement_keys = ["w", "a", "s", "d"]
+            if self.player.cooldown > 0:
+                self.player.cooldown -= 1
             if kb.kbhit():
                 c = kb.getch()
-                if c in movement_keys:
-                    self.player.move(c)
-
                 if ord(c) == 27: # ESC
                     break
+                if self.player.cooldown <= 0:
+                    self.player.process_command(c)
+
             for enemy in self.enemies:
                 enemy.act(self.frame)
+            for weapon in self.weapons:
+                weapon.act(self.frame)
             wait_time = max([0, frame_time - (time.time() - start_time)])
             time.sleep(frame_time)
 
